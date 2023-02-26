6acec4deab7f9b5265bbddace68c79aeba0c1eb5
ettorto <111897917+ettorto@users.noreply.github.com>
2023-02-26 20:36:25 +0000

    done

--- clinic_db.sql
+++ clinic_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2023 at 11:50 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+-- Generation Time: Feb 26, 2023 at 09:33 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,8 @@ INSERT INTO `all_doctor` (`Doctor_Id`, `Firstname`, `Password`) VALUES
 ('user_Id', 'Adjei_Patrick', '$2y$10$Sn2gI5sqQzW8upaEL7Wfuey'),
 ('user_Id', 'Adjei_', '$2y$10$FHct2YSbsGwxLOIKY6VovOu'),
 ('user_Id', 'Manu_Peter', '$2y$10$5leTXZKhAMex0eYzIdt9Tez'),
-('D785456', 'Manu_Dav', '$2y$10$Fo9tWs2lKNH16B/Pr4gYdOR');
+('D785456', 'Manu_Dav', '$2y$10$Fo9tWs2lKNH16B/Pr4gYdOR'),
+('12345', 'Micheal', '$2y$10$rLEsc30WcM6D43bHDWej5OV');
 
 -- --------------------------------------------------------
 
@@ -95,6 +96,51 @@ INSERT INTO `doctor` (`Doctor_Id`, `Firstname`, `Lastname`, `Gender`, `Room_offi
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `doctors_appointments`
+--
+
+CREATE TABLE `doctors_appointments` (
+  `assigneeID` int(11) NOT NULL,
+  `doctorID` int(11) NOT NULL,
+  `patientID` int(11) NOT NULL,
+  `appointment_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `appointment_status` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `emplyee_id` int(11) NOT NULL,
+  `FirstName` varchar(20) NOT NULL,
+  `LastName` varchar(25) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `emp_pass` varchar(255) NOT NULL,
+  `PhoneNumber` varchar(255) NOT NULL,
+  `Gender` varchar(6) NOT NULL,
+  `emp_role` varchar(1) NOT NULL,
+  `Specialization` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`emplyee_id`, `FirstName`, `LastName`, `Email`, `emp_pass`, `PhoneNumber`, `Gender`, `emp_role`, `Specialization`) VALUES
+(11, 'Ernestina', 'Torto', 'ernest.torto@clinic.com', '$2y$10$30Stq58GfvbtAJqHdnR4LeoPfRVyb91RRs4O6HiMuCB65.3obYsd.', '1234567', 'female', 'd', 'Immunology'),
+(12, 'Dorcas', 'Torto', 'dorcas.adjei@clinic.com', '$2y$10$Ubo.aDMXYN7Hds3HaZt7f.AS4lhKHToU8hhw0w93Gz8yW1ONY8Z7S', '23456789098755', 'female', 'a', NULL),
+(13, 'Dorcas', 'Torto', 'dorcas.adjei@clinic.com', '$2y$10$coli8TDswx7L6OKHlimFguVkYpfp0Vt0ihakPogwoM6B6Ixgt2zKC', '23456789098755', 'female', 'a', NULL),
+(14, 'Ernestina', 'Adjei', 'ernestina.adjei@clinic.com', '$2y$10$lZABX38CLz7xGCVpVdrWGOk3Er.94e.7ac8BDP9FLG4mNIm8LaA5a', '23456789098755', 'female', 'a', 'none');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `nurses`
 --
 
@@ -117,6 +163,49 @@ INSERT INTO `nurses` (`Fname`, `Lname`, `Nurse_Id`, `Tel_number`) VALUES
 ('Perpetual', 'Ofori-Ampofo', 'N560-33', 20154784),
 ('Monica', 'Afra Nkrumah', 'N560-51', 502771331);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nurses_tasks`
+--
+
+CREATE TABLE `nurses_tasks` (
+  `assigneeID` int(11) NOT NULL,
+  `nursesID` int(11) NOT NULL,
+  `task_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `task_status` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `user_role` int(11) NOT NULL,
+  `user_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_table`
+--
+
+INSERT INTO `user_table` (`user_id`, `user_name`, `user_pass`, `user_role`, `user_status`) VALUES
+(1, 'davesam', '$2y$10$wSKcAzj.BTJPWWvk2WHt1eZAhbN5u3VGhZVEQffmFp4SExJ1xQ3K.', 1, 1),
+(3, 'samdave', '$2y$10$7J4TB.GEoy3vNkAVAqVYs.wgPKu1zsdaQ1.vivCID8LkR0eNgpSqS', 2, 1),
+(4, 'olduser', '$2y$10$3JIDyaax5TsiYLdKfRJQIO.c6waaraufi7knuJvUlptcKNKurQLpS', 2, 2),
+(9, 'ernestt@gmail.com', '$2y$10$zYjHSNeNDBQOidCFpDomXeIFM6ltfyVkHOuZodEKnMjq0CPPcITcS', 1, 1),
+(10, 'doc@clinc.com', '$2y$10$g8N7QCEveShWnfb74wG9KuHjW1Yn2bLkEGEHw7L50tqBPgJMu4iW6', 2, 1),
+(11, 'nurse@clinc.com', '$2y$10$2CuLin.7rfDqwuJe9.L8muArEs4TdLlyevgKi6bsIe8mm1xEkO6vm', 3, 1),
+(13, 'qwergh@asww.com', '$2y$10$vUGzaBTYR/74P9b8eQ.bmOePPy8TpmUF3gcYJ7zdzbuCoh99emjAy', 1, 1);
+
 --
 -- Indexes for dumped tables
 --
@@ -126,6 +215,47 @@ INSERT INTO `nurses` (`Fname`, `Lname`, `Nurse_Id`, `Tel_number`) VALUES
 --
 ALTER TABLE `all_nurses`
   ADD UNIQUE KEY `Nurse_Id` (`Nurse_Id`);
+
+--
+-- Indexes for table `doctors_appointments`
+--
+ALTER TABLE `doctors_appointments`
+  ADD PRIMARY KEY (`appointment_date`,`start_time`,`end_time`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emplyee_id`);
+
+--
+-- Indexes for table `nurses_tasks`
+--
+ALTER TABLE `nurses_tasks`
+  ADD PRIMARY KEY (`start_time`,`end_time`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emplyee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
